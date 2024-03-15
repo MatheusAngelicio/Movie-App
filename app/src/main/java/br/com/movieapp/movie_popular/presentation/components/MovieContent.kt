5efc54fcd9dd210a497b8b72f0e09fb62f5622b1
Matheus Angelicio <matheus.angelicio00@gmail.com>
2024-03-15 13:53:00 -0300

    load and error treatment

--- app/src/main/java/br/com/movieapp/movie_popular/presentation/components/MovieContent.kt
+++ app/src/main/java/br/com/movieapp/movie_popular/presentation/components/MovieContent.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
@@ -13,8 +14,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import br.com.movieapp.core.domain.model.Movie
+import br.com.movieapp.core.presentation.components.common.ErrorScreen
+import br.com.movieapp.core.presentation.components.common.LoadingView
 
 @Composable
 fun MovieContent(
@@ -48,6 +52,38 @@ fun MovieContent(
                             onClick(movieId)
                         }
                     )
+                }
+
+            }
+
+            pagingMovies.apply {
+                when {
+                    loadState.refresh is LoadState.Loading || loadState.append is LoadState.Loading -> {
+                        item(
+                            //GridItemSpan- explicação
+//item(span = { GridItemSpan(maxLineSpan) }): Essa linha define um item na grade (grid) onde a visualização de carregamento será exibida.
+//A função span é usada para especificar o número de colunas que o item ocupa na grade.
+//No caso, GridItemSpan(maxLineSpan) retorna um objeto SpanSize, indicando que o item deve ocupar um número máximo de linhas (maxLineSpan) na grade.
+                            span = {
+                                GridItemSpan(maxLineSpan)
+                            }
+                        ) {
+                            LoadingView()
+                        }
+                    }
+
+                    loadState.refresh is LoadState.Error || loadState.append is LoadState.Error -> {
+                        item(
+                            span = {
+                                GridItemSpan(maxLineSpan)
+                            }
+                        ) {
+                            ErrorScreen(message = "Verifique sua conexão com a internet") {
+                                retry()
+                            }
+                        }
+
+                    }
                 }
 
             }
